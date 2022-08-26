0d84eb701ebc54e5a758a9472fce8839b774a5dd
Anton Rybak <toxa-wow@mail.ru>
2022-08-26 18:08:58 +0300

    task 19 done

---
+++ task19/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число ");
+int num = int.Parse(Console.ReadLine());
+string s = Convert.ToString(num);
+if (s[0] == s[4] && s[1] == s[3]){
+    Console.WriteLine("Число палиндром");
+}
+else Console.WriteLine("Число не является палиндромом");
