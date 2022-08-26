ed3e734c60d0c9fae13540a5f5ff7061bfe38960
Anton Rybak <toxa-wow@mail.ru>
2022-08-26 18:43:50 +0300

    task 23 done

---
+++ task23/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= num){
+    Console.WriteLine(Math.Pow (count, 3));
+    count ++;
+}
